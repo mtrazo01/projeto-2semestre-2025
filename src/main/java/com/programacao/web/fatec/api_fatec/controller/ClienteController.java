7390f532bb6c4af2a7ae4d68294507a8810dfba6
mtrazo01 <mtrazo01@gmail.com>
2025-08-20 19:16:16 -0300

    feat(); criacao classe cliente

---
+++ src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
@@ -0,0 +1,23 @@
+package com.programacao.web.fatec.api_fatec.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteController {
+
+    @GetMapping("/testeCliente1")
+    public String testeCliente() {
+        return "Teste Cliente";
+    }
+    @GetMapping("/testeCliente2/{nome}")
+    public String testeCliente2(@PathVariable String nome) {
+        return nome;
+    }
+
+    
+    
+}
---
+++ src/main/java/com/programacao/web/fatec/api_fatec/entities/Cliente.java
@@ -0,0 +1,6 @@
+package com.programacao.web.fatec.api_fatec.entities;
+
+public class Cliente {
+    private Long id;
+    private String nome; 
+}
